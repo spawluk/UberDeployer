85a55a50090af10bfa4ce95ba5bfacc24d34a12a
mariusz-rubin <mariusz.rubin@gmail.com>
2015-03-10 11:12:16 +0100

    password collection in env deploy

--- Src/UberDeployer.WebApp/Static/js/EnvDeploy.js
+++ Src/UberDeployer.WebApp/Static/js/EnvDeploy.js
@@ -25,10 +25,10 @@ UberDeployer.EnvDeploy = function() {
 
   var loadProjectsForCurrentEnvironmentDeploy = function() {
     clearProjects();
-    
+
     $.getJSON(g_AppPrefix + 'EnvDeployment/GetProjectsForEnvironmentDeploy', { environmentName: getSelectedTargetEnvironmentName() })
       .done(
-        function (data) {
+        function(data) {
           _projects = [];
           clearProjects();
 
@@ -43,6 +43,89 @@ UberDeployer.EnvDeploy = function() {
           });
         });
   };
+
+  var setupSignalR = function() {
+    var deploymentHub = $.connection.deploymentHub;
+
+    deploymentHub.client.connected = function() {
+    };
+    deploymentHub.client.disconnected = function() {
+    };
+
+    deploymentHub.client.promptForCredentials =
+      function(message) {
+        showCollectCredentialsDialog(
+          message.deploymentId,
+          message.projectName,
+          message.projectConfigurationName,
+          message.targetEnvironmentName,
+          message.machineName,
+          message.username);
+      };  
+
+    deploymentHub.client.cancelPromptForCredentials =
+      function() {
+        closeCollectCredentialsDialog();
+      };
+
+    $.connection.hub.start();
+  };
+
+  var showCollectCredentialsDialog = function(deploymentId, projectName, projectConfigurationName, targetEnvironmentName, machineName, username) {
+    $('#dlg-collect-credentials-deployment-id').val(deploymentId);
+    $('#dlg-collect-credentials-project-name').html(projectName);
+    $('#dlg-collect-credentials-project-configuration-name').html(projectConfigurationName);
+    $('#dlg-collect-credentials-target-environment-name').html(targetEnvironmentName);
+    $('#dlg-collect-credentials-machine-name').val(machineName);
+    $('#dlg-collect-credentials-username').val(username);
+    $('#dlg-collect-credentials-password').val('');
+
+    $('#dlg-collect-credentials').modal('show');
+  };
+
+  var closeCollectCredentialsDialog = function() {
+    $('#dlg-collect-credentials-deployment-id').val('');
+    $('#dlg-collect-credentials-project-name').html('');
+    $('#dlg-collect-credentials-project-configuration-name').html('');
+    $('#dlg-collect-credentials-target-environment-name').html('');
+    $('#dlg-collect-credentials-machine-name').val('');
+    $('#dlg-collect-credentials-username').val('');
+    $('#dlg-collect-credentials-password').val('');
+
+    $('#dlg-collect-credentials').modal('hide');
+  };
+
+  var setupCollectCredentialsDialog = function() {
+    $('#dlg-collect-credentials-ok')
+      .click(function() {
+        var deploymentId = $('#dlg-collect-credentials-deployment-id').val();
+        var password = $('#dlg-collect-credentials-password').val();
+
+        if (password === '') {
+          alert('You have to enter the password.');
+          return;
+        }
+
+        $.ajax({
+          url: g_AppPrefix + 'InternalApi/OnCredentialsCollected',
+          type: "POST",
+          data: {
+            deploymentId: deploymentId,
+            password: password,
+          },
+          traditional: true
+        });
+
+        closeCollectCredentialsDialog();
+      });
+
+    $('#dlg-collect-credentials')
+      .on(
+        'shown',
+        function() {
+          $('#dlg-collect-credentials-password').focus();
+        });
+  };  
 
   return {
     initializeEnvDeploymentPage : function(initData) {
