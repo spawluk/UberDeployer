2a1d8bab5a187ad796abd41e7472d45c562c8cc7
mariusz-rubin <mariusz.rubin@gmail.com>
2015-07-17 09:48:15 +0200

    Fix Skip dependencies

--- Src/UberDeployer.Core/ExternalDataCollectors/DependentProjectsSelection/DependentProjectsToDeploySelection.cs
+++ Src/UberDeployer.Core/ExternalDataCollectors/DependentProjectsSelection/DependentProjectsToDeploySelection.cs
@@ -4,6 +4,11 @@ namespace UberDeployer.Core.ExternalDataCollectors.DependentProjectsSelection
 {
   public class DependentProjectsToDeploySelection
   {
+    public DependentProjectsToDeploySelection()
+    {
+      SelectedProjects = new List<DependentProject>();
+    }
+
     public List<DependentProject> SelectedProjects { get; set; }
   }
 }
